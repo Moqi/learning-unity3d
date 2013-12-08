dc941da614c0e4917a0a9f75cf7686d348a0448f
yanzs <danrenjian@gmail.com>
2013-12-08 17:37:38 +0800

    use button and slider
    the learning scene has a game object that is created using the Driving
    prefab, which contains a button and a slider. the button can change the
    display of the slider by clicking and properties set on it

---
+++ Assets/Scripts/Driver.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Driver : MonoBehaviour {
+    public MakeProgress makeProgress;
+    public int curValue;
+    public int maxValue;
+    public bool labelVisible;
+	
+	// Update is called once per frame
+	void OnClick () {
+        makeProgress.LabelVisible = labelVisible;
+        makeProgress.SetText(curValue.ToString() + "/" + maxValue.ToString ());
+        makeProgress.UpdateProgress(curValue / (float)maxValue);
+	}
+}
---
+++ Assets/Scripts/MakeProgress.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeProgress : MonoBehaviour {
+    public UISlider slider;
+    public UILabel label;
+
+    private float _sliderWidth;
+
+	// Use this for initialization
+	void Awake () {
+        _sliderWidth = slider.foreground.localScale.x;
+	}
+	
+	// Update is called once per frame
+	public void UpdateProgress (float per) {
+        Vector3 localScale = slider.foreground.localScale;
+        slider.foreground.localScale = new Vector3(_sliderWidth * per, localScale.y, localScale.z);
+	}
+
+    public void SetText(string str)
+    {
+        label.text = str;
+    }
+
+    public bool LabelVisible
+    {
+        set
+        {
+            label.enabled = value;
+        }
+    }
+}
