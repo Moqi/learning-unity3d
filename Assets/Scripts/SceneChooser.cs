82d06c23428a63ec6465453bb20ef913961cfeb7
yanzs <danrenjian@gmail.com>
2013-12-08 18:26:33 +0800

    switch between scenes
    run the MainMenu scene, and click the "Show Progress" button, the
    Progress scene will be shown.
    I have add Progress scene in the build settigns

---
+++ Assets/Scripts/SceneChooser.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChooser : MonoBehaviour {
+
+	void ShowProgressScene()
+    {
+        Application.LoadLevel("Progress");
+    }
+
+    void DoSomethingElse()
+    {
+        print ("tell me what to do");
+    }
+}
