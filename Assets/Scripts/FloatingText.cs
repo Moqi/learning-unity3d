b2e5b78ed1eed13c9b20181d11945559861d6426
yanzs <danrenjian@gmail.com>
2013-12-09 09:43:05 +0800

    floating text

---
+++ Assets/Scripts/FloatingText.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingText : MonoBehaviour {
+    private UILabel _label;
+
+    void Awake()
+    {
+        _label = GetComponent<UILabel>();
+        Color = Color.red;
+        Text = "good to go";
+    }
+
+    public Color Color
+    {
+        get
+        {
+            return _label.color;
+        }
+        set
+        {
+            _label.color = value;
+        }
+    }
+
+    public string Text
+    {
+        get
+        {
+            return _label.text;
+        }
+        set
+        {
+            _label.text = value;
+        }
+    }
+}
